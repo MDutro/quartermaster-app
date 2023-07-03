9da73353db3452f62602266849a37fdd465234a6
Dutro <mdutrolaw@gmail.com>
2023-07-03 08:31:40 -0700

    Stub out basic React app/components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import { Products } from './components/Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to='/products'>Products</Link>
+            </li>
+          </ul>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' />
+            <Route path='/products' element={<Products/>} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
