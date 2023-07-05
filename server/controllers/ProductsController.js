97042bb73f69d2120ba2fb5a1e44c177b69c4992
Dutro <mdutrolaw@gmail.com>
2023-07-05 13:53:20 -0700

    Add more product routes/methods

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -1,16 +1,51 @@
-const products = require('express').Router();
-const db = require('../models');
+const products = require("express").Router();
+const db = require("../models");
 const { Product } = db;
 
 // get all products
-products.get('/', async (req, res) => {
-    try {
-        const fetchedProducts = await Product.findAll();
-        res.status(200).json(fetchedProducts);
-    } catch (err) {
-        res.status(500).json("Server error");
-        console.log(err);
-    }
-})
+products.get("/", async (req, res) => {
+  try {
+    const fetchedProducts = await Product.findAll();
+    res.status(200).json(fetchedProducts);
+  } catch (err) {
+    res.status(500).json("Server error");
+    console.log(err);
+  }
+});
+
+// get one product
+products.get("/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    console.log(id);
+    let product = await Product.findOne({ where: { id: id } });
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(400).json("Product not found");
+    console.log(err);
+  }
+});
+
+products.put("/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    const product = await Product.update(req.body, { where: { id: id } });
+    res.status(200).send(product);
+  } catch (err) {
+    res.status(400).json("Product not found");
+    console.log(err);
+  }
+});
+
+products.delete("/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    await Product.destroy({ where: { id: id } });
+    res.status(200).send("Product is deleted !");
+  } catch (err) {
+    res.status(400).json("Product not found or already deleted");
+    console.log(err);
+  }
+});
 
 module.exports = products;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-//const { Seqelize } = require('sequelize');
-//const path = require('path');
-const cors = require('cors');
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-const productsController = require('./controllers/ProductsController');
-app.use('/api/products', productsController);
+const productsController = require("./controllers/ProductsController");
+app.use("/api/products", productsController);
 
 app.listen(4005, () => {
-    console.log("Server is running on port 4005");
+  console.log("Server is running on port 4005");
 });
