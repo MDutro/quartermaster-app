d13fdd5c702eb1d1ed2b6d414db0201d54c99c77
Dutro <mdutrolaw@gmail.com>
2023-07-07 11:50:32 -0700

    Remove context-related vestiges

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useEffect, useContext } from "react";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
 import Topbar from "./views/global/Topbar";
 import Sidebar from "./views/global/Sidebar";
 import { ProductsTable } from "./views/products";
-import { IProduct } from "./types/product";
-import ProuctDataService from "./services/product.service";
-import { useAppSelector, useAppDispatch } from "./state/hooks";
+import { useAppDispatch } from "./state/hooks";
 import {
-  allProducts,
   fetchProducts,
 } from "./state/features/product/productSlice";
 
 function App() {
   const [theme, colorMode] = useMode();
-  const products = useAppSelector(allProducts);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     console.log("@@@ useEffect running!!! @@@");
     const fetchData = async () => {
-      const response = await ProuctDataService.getAll();
-      //setProductsContext(response.data);
       dispatch(fetchProducts());
     };
     fetchData();
-  }, []);
+  }, [dispatch]);
 
   return (
     <ColorModeContext.Provider value={colorMode as any}>
